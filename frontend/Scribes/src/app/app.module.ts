9824147ca9103fed523482f8fd191500cb6587d5
Hassen Kaabouri <kaabouri.hassen704@gmail.com>
2021-11-07 22:21:17 +0100

    added routers links to navbar

--- frontend/Scribes/src/app/app.module.ts
+++ frontend/Scribes/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router'
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  
+  {path: 'login',component: LoginComponent},
+  {path: 'profil',component: ProfileComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavBarComponent,
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
