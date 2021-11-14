e09f27f8459c9163825b22fccf517f7e82e2dfb1
Hassen Kaabouri <kaabouri.hassen704@gmail.com>
2021-11-14 16:26:43 +0100

    Flutter: login home pages

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,24 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'app.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-    );
-  }
-}
+void main() => runApp(const ScribesApp());
 
 
 
