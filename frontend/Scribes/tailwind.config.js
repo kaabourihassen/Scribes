588125ba0d896433f59f16ad15b2d8194bed99ee
bejaoui oussama <bejaouioussama556@gmail.com>
2021-11-15 01:59:23 +0100

    added the register template

--- frontend/Scribes/tailwind.config.js
+++ frontend/Scribes/tailwind.config.js
@@ -515,6 +515,7 @@ module.exports = {
       ...theme('spacing'),
       full: '100%',
       screen: '100vh',
+      80:'80vh',
     }),
     maxWidth: (theme, { breakpoints }) => ({
       none: 'none',
